89f8142b4f07105a5fc53cbe45c8417bba81ce48
Grace Johnson <grace.johnson@flutteruki.com>
2024-08-15 08:05:36 +0100

    Testing git credentials

--- src/main/java/com/bngcalculator/Controller/Controller.java
+++ src/main/java/com/bngcalculator/Controller/Controller.java
@@ -10,6 +10,6 @@ public class Controller {
    @CrossOrigin(origins = "http://localhost:3000")
    @GetMapping("/health")
     public String getHealthCheck() {
-        return "You got your endpoint";
+        return "You hit the endpoint";
     }
 }
