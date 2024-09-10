ce4bea346df786de76fca6de8f181493ffa4d8d2
Grace Johnson <grace.johnson@flutteruki.com>
2024-09-10 15:42:14 +0100

    [2] receiving value from front end and manipulating to send back

--- src/main/java/com/bngcalculator/Controller/Controller.java
+++ src/main/java/com/bngcalculator/Controller/Controller.java
@@ -1,15 +1,35 @@
 package com.bngcalculator.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
+@RequestMapping
 public class Controller {
 
-   @CrossOrigin(origins = "http://localhost:3000")
-   @GetMapping("/health")
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/health")
     public String getHealthCheck() {
         return "You have reached the endpoint";
+    }
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/postnumber")
+    public String submitValue(@RequestBody ValueRequest request) {
+        int value = request.getValue();
+        value = value*2;
+        return "Your value x 2 = " + value;
+    }
+}
+
+class ValueRequest {
+    private int value;
+
+    // Getter and setter
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 }
